427e3bf4924c77a13bfb8febe3d862d9c7e21010
Wei Jie <25047684@myrp.edu.sg>
2026-07-16 10:40:35 +0800

    udpatedd

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
     host: 'c237-hannah-mysql.mysql.database.azure.com',
     user: 'c237_016',
     password: 'c237016@2026!',
-    database: 'C237_016_t3regapp',
+    database: 'C237_016_t3supdb',
     ssl: {
         rejectUnauthorized: false
     }
